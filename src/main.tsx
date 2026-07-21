fc185adf8000ef80ac21d7d899fb377ecd6fd4e4
humuchen <humuchen@users.noreply.github.com>
2026-07-21 05:19:12 +0000

    feat: Vercel部署页面未显示
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ document.title = APP_CONFIG.name;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/medical-aesthetics-agent">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
